0087b74ac4c6ff89b2e6b368fdaeefe2d6139c53
Imasha <Imasha@SILVERBUNNY>
2024-04-28 00:28:48 +0530

    Add project files.

---
+++ Lab 01 Q5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to display its multiplication table:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Multiplication table of {number}:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} x {i} = {result}");
+            }
+        }
+    }
+}
